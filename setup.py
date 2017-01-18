943fedcae9dc40453030291b6fc32a941424dc57
jensenzhang <jingxuan.n.zhang@gmail.com>
2017-01-18 19:36:58 +0800

    Add more packaging information
    Add executable scripts under `bin` directory into the package.
    
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from os import path, listdir
+
+scripts = [path.join('bin', filename) for filename in os.listdir('bin')]
 
 setup(name='sdntest',
       version='0.1',
@@ -6,6 +9,24 @@ setup(name='sdntest',
       url='http://github.com/snlab-freedom/sdntest',
       author='Jensen Zhang',
       author_email='jingxuan.n.zhang@gmail.com',
+      long_description="""
+          SDNTest is a general purpose test framework for SDN scenario.
+          It allows users to define their own testcases and repeatedly
+          execute them in different docker containers.
+          """,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: 2 - Pre-Alpha",
+          "Intended Audience :: Developers",
+          "Topic :: System :: Networking :: Test Framework",
+          "Topic :: Utilities"
+      ],
       license='MIT',
-      packages=['sdntest'],
+      packages=['sdntest', 'sdntest.example'],
+      install_requires=[
+          'setuptools',
+          'docker'
+      ],
+      scripts=scripts,
       zip_safe=False)
